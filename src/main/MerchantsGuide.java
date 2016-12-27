5086aca915023c73e89b91321d97c61d854b49a1
Deshpande, Shalaka (DS) <Shalaka.Deshpande@cdk.com>
2016-12-27 12:15:53 +0530

    Moved App exe file to main pckg

---
+++ src/main/MerchantsGuide.java
@@ -0,0 +1,41 @@
+package main;
+
+import inputReader.InputReaderFactory;
+import inputReader.Reader;
+
+import java.util.List;
+
+import processor.Processor;
+import processor.ProcessorFactory;
+import writer.WriterFactory;
+
+public class MerchantsGuide {
+
+	public String parseInputNotes() {
+		Reader reader = InputReaderFactory.getInputReader("FILE");
+		List<String> fileContents = reader.read();
+
+		Processor processor = ProcessorFactory.getProcessor("GALAXY");
+		String outputToFile = processor.process(fileContents);
+
+		return outputToFile;
+	}
+
+	public static void main(String[] args) {
+		MerchantsGuide interGalacticConverter = new MerchantsGuide();
+		String outputToFile = interGalacticConverter.parseInputNotes();
+
+		boolean success = writeToFile(outputToFile);
+
+		if (success) {
+			System.out
+					.println("SuccessFully Written output to output.txt file");
+		} else {
+			System.out.println("Failed to write output to file");
+		}
+	}
+
+	private static boolean writeToFile(String outputToFile) {
+		return WriterFactory.getWriter("FILE").write(outputToFile);
+	}
+}
